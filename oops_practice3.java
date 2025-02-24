a0b8b57196665986952eb7662233fa4871ce36a9
AnuragDhull <141478435+AnuragDhull@users.noreply.github.com>
2025-02-24 18:24:06 +0530

    Add files via upload

---
+++ oops_practice3.java
@@ -0,0 +1,36 @@
+package  OOPs.Basics;
+
+import java.util.Scanner;
+
+class rectangle{
+    int lenght;
+    int breadth;
+
+public int area ()
+{
+    return lenght*breadth;
+}
+public int perimeter()
+{
+    return 2*(lenght+breadth);
+}
+}
+
+public class oops_practice3 {
+    public static void main(String[] args) {
+        rectangle r1=new rectangle();
+        
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the lenght of rectangle: ");
+        r1.lenght=sc.nextInt();
+        System.out.println("Enter the breadt of rectangle: ");
+        r1.breadth=sc.nextInt();
+
+
+       System.out.println(r1.area());
+       System.out.println(r1.perimeter());
+
+
+       sc.close();
+    }
+}
